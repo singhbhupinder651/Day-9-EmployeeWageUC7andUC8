668bdc95c7a790e57aeed9f91800f40a823b6646
singhbhupinder651 <71844883+singhbhupinder651@users.noreply.github.com>
2022-10-26 23:09:25 +0530

    updated

---
+++ EmployeeWageMultipleCompanies.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class EmployeeWageMultipleCompanies
+    {
+        const int PRESENT_FULL_TIME = 1;
+        const int PRESENT_PART_TIME = 2;
+
+        public int CalculateMonthlyWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            Random random = new Random();
+
+            int totalEmpWage;
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+
+            while (totalWorkingDays < numOfWorkingDays && totalEmpHrs <= maxHoursPerMonth)
+            {
+                totalWorkingDays++;
+                int check = random.Next(0, 3);
+
+                switch (check)
+                {
+                    case PRESENT_FULL_TIME:
+                        Console.WriteLine("Employee Present: Full Time");
+                        empHrs = 8;
+                        break;
+                    case PRESENT_PART_TIME:
+                        Console.WriteLine("Employee Present: Part Time");
+                        empHrs = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Employee Absent");
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#:" + totalWorkingDays + " Emp Hrs : " + empHrs);
+            }
+            totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine("Total Emp Wage for company : " + company + " is: " + totalEmpWage);
+
+            return totalEmpWage;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace EmployeeWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            Console.WriteLine("Use Case #8: Employee Wage for Multiple Companies");
+            Console.Write("Choose a use case number to run the program: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 8:
+                    EmployeeWageMultipleCompanies employeeWageMultipleCompanies = new EmployeeWageMultipleCompanies();
+                    employeeWageMultipleCompanies.CalculateMonthlyWage("Flipkart", 20, 10, 60);
+                    employeeWageMultipleCompanies.CalculateMonthlyWage("Amazon", 20, 15, 80);
+                    break;
+                default:
+                    Console.WriteLine("Please enter a valid use case number!");
+                    break;
+            }
+        }
+    }
+}
